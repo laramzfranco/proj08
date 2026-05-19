157ccea486d4196515d2b766dbffda8fde1f0dcf
laramzfranco <laramzfranco@gmail.com>
2026-05-19 10:58:20 -0300

    proj08

---
+++ script.js
@@ -0,0 +1,8 @@
+const aluno =  [{name: "Letycia", Idade: 16, DiciplinaPreferida: "Portugues"},
+                 {name: "Lidia", Idade: 16, DiciplinaPreferida: "Portugues"},
+                 {name: "Ligya", Idade: 16, DiciplinaPreferida: "Educação Fisica"},
+                 {name: "Ryana", Idade: 16, DiciplinaPreferida: "História"}, 
+                 {name: "Felipe", Idade: 16, DiciplinaPreferida: "Educação Fisica"}]
+
+alert("Primeiro aluno: " + alunos[0].nome);
+alert("Diciplina preferida do ultimo aluno: " + alunos[4].diciplinapreferida);
